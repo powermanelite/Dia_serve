19bcbc4712cada822ab5db7c79a67dbea7fd6fa6
Diamond Phu <diphu@ucsc.edu>
2026-04-17 15:27:48 -0700

    edit deploy state

--- my-website/src/main.tsx
+++ my-website/src/main.tsx
@@ -4,12 +4,16 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css';
 import App from './App';
 
-const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID as string;
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID as string | undefined;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={clientId}>
+    {clientId ? (
+      <GoogleOAuthProvider clientId={clientId}>
+        <App />
+      </GoogleOAuthProvider>
+    ) : (
       <App />
-    </GoogleOAuthProvider>
+    )}
   </StrictMode>
 );
